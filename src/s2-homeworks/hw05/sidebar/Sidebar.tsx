2e0ec5dede0d6662a486f923766a34b650a337dd
DimaSadovsky21 <dimasadovsky21@gmail.com>
2023-01-05 16:19:00 +0300

    hw5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -12,7 +12,6 @@ type PropsType = {
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
-        const activePage = open ? s.active : s.a
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -32,7 +31,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={activePage} // делает студент
+                        className={({isActive}) => isActive ? s.active : s.a} // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -40,7 +39,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={activePage} // делает студент
+                        className={({isActive}) => isActive ? s.active : s.a} // делает студент
                     >
                         Junior
                     </NavLink>
@@ -48,7 +47,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={activePage} // делает студент
+                        className={({isActive}) => isActive ? s.active : s.a } // делает студент
                     >
                         Junior Plus
                     </NavLink>
